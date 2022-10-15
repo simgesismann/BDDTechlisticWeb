6f9721255f4e2ce1adcba8fc31f0bdb77ba1bdeb
simgesismann <62858275+simgesismann@users.noreply.github.com>
2022-10-15 23:23:46 +0300

    runner general is added

---
+++ src/main/java/Runners/RunnerGeneral.java
@@ -0,0 +1,14 @@
+package Runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = {"src/test/java"},
+        glue = {"stepDefinitions"})
+
+public class RunnerGeneral {
+
+}
